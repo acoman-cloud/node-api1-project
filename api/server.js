59a83a0dbd18830dc74a02670306f1e7d69dc081
Anthony Coman <acoman@yahoo.com>
2022-01-12 18:54:41 -0600

    finally got it working normally

--- api/server.js
+++ api/server.js
@@ -1,3 +1,28 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const User = require('./users/model.js')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+server.get('/api/users', (req, res) =>{
+	res.json('users');
+})
+
+
+server.post('/api/users', async (req, res) => {
+  try {
+    const { name, bio } = req.body
+    console.log(name, bio)
+    const newUser = await User.create({ name, bio })
+    console.log(newUser)
+    res.status(201).json(newUser) // :(
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
+
+server.use('*', (req, res) =>{
+	res.status(404).json({ message: "not found" })
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
